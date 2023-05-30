ef4c2ccfa0531a54f8d41c0f1e0a5c017cdd5fbf
Tomasz Wysoczański <t.wysoczanski17@gmail.com>
2023-05-30 18:45:37 +0200

    feat: update About and PageNotFound pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,8 @@ function About() {
     return (<section>
         <h1>About</h1>
         <hr className="decorative-hrule" />
-        <p>Let's face it, one of the main reasons the internet was created was to allow people to send pictures of funny cats.</p>
-        <p>Embrace the truth and dive into the biggest collection of cat memes on the Internet.</p>
+        <p>Let's face it, one of the main reasons the Internet was created was to allow people to send pictures of funny cats.</p>
+        <p>Embrace the truth, buckle up and dive into the ultimate collection of cat memes on the Internet!</p>
     </section>)
 }
 
--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,8 +1,11 @@
 function PageNotFound() {
     return (<section>
-        <h1>Oops!</h1>
+        <h1>Oops! Page not found</h1>
         <hr className="decorative-hrule" />
-        <p>Page not found</p>
+        <div className="img-404-container">
+            <img className="img-404" src="page-not-found.png" alt="decorative" />
+        </div>
+        <p>Oh no! The page you are looking for has been annihilated by the angry space cat!</p>
     </section>)
 }
 
