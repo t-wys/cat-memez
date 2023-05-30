6e0b4babafddf152715a24d274f6a33abdb52d89
Tomasz Wysoczański <t.wysoczanski17@gmail.com>
2023-05-30 17:06:33 +0200

    feat: add minimun number of votes to best meme check

--- src/pages/BestMemes.js
+++ src/pages/BestMemes.js
@@ -8,6 +8,7 @@ function BestMemes() {
         <h1>Top-notch stuff</h1>
         <hr className="decorative-hrule" />
         <p>Only the best memez, approved by community</p>
+        <p>The meme must get <em>100+ votes</em> with <em>over 66% upvotes</em> to claim its spot among the best</p>
         <div>
             {memeList.toSorted((a, b) => (b.rating / b.votesCount) - (a.rating / a.votesCount)).map(meme => <Meme meme={meme} key={meme.id} />)}
         </div>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,6 @@
 const votesRatio = 1.5;
+const minVotes = 100;
 
 export const checkMeme = (rating, votesCount) => {
-    return rating * votesRatio > votesCount;
+    return votesCount >= minVotes && rating * votesRatio > votesCount;
 }
